63dd3c73714087f12684096bf12d7a04e5018247
ElovikovStepan <selovikov12345@gmail.com>
2024-11-21 19:27:09 +0500

    Добавленные переменные сервера. Еловиков-Галкин

--- Snake_Elovikov_Galkin/Program.cs
+++ Snake_Elovikov_Galkin/Program.cs
@@ -8,6 +8,11 @@ namespace Snake_Elovikov_Galkin
 {
 	internal class Program
 	{
+		public static List<Leaders> leaders = new List<Leaders();
+		public static List<ViewModelUserSettings> remoteIPAddress = new List<ViewModelUserSettings>();
+		public static List<ViewModelGames> viewModelGames = new List<ViewModelGames>();
+		public static int localPort = 5001;
+		public static int maxSpeed = 15;
 		static void Main(string[] args)
 		{
 		}
