390602e08a3d926abdd6f0589f4a83e4920321e2
ElovikovStepan <selovikov12345@gmail.com>
2024-11-21 20:08:22 +0500

    Исправлены ошибки серверной части. Еловиков-Галкин

--- Snake_Elovikov_Galkin/Program.cs
+++ Snake_Elovikov_Galkin/Program.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Common;
+using Newtonsoft.Json;
 
 namespace Snake_Elovikov_Galkin
 {
@@ -35,23 +36,26 @@ namespace Snake_Elovikov_Galkin
 		}
 		private static void Send()
 		{
-			UdpClient sender = new UdpClient();
-			IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(User.IPAddress), int.Parse(User.Port));
-			try
+			foreach (ViewModelUserSettings User in remoteIPAddress)
 			{
-				byte[] bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(viewModelGames.Find(x => x.IdSnake == User.IdSnake)));
-				sender.Send(bytes, bytes.Length, endPoint);
-				Console.ForegroundColor = ConsoleColor.Yellow;
-				Console.WriteLine($"Отправил данные пользователю: {User.IPAddress}:{User.Port}");
-			}
-			catch (Exception ex)
-			{
-				Console.ForegroundColor = ConsoleColor.Red;
-				Console.WriteLine($"Возникло исключение: {ex.ToString()}\n{ex.Message}");
-			}
-			finally
-			{
-				sender.Close();
+				UdpClient sender = new UdpClient();
+				IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(User.IPAddress), int.Parse(User.Port));
+				try
+				{
+					byte[] bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(viewModelGames.Find(x => x.IdSnake == User.IdSnake)));
+					sender.Send(bytes, bytes.Length, endPoint);
+					Console.ForegroundColor = ConsoleColor.Yellow;
+					Console.WriteLine($"Отправил данные пользователю: {User.IPAddress}:{User.Port}");
+				}
+				catch (Exception ex)
+				{
+					Console.ForegroundColor = ConsoleColor.Red;
+					Console.WriteLine($"Возникло исключение: {ex.ToString()}\n{ex.Message}");
+				}
+				finally
+				{
+					sender.Close();
+				}
 			}
 		}
 		public static void Receiver()
